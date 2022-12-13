6bd0937f656ed3bfedda85534f1e85a04fd21340
Cindy Alejandra Sepulveda Cadavid <cindysepulveda@comfama.com.co>
2022-12-12 23:23:17 -0500

    Prueba de inicio rápido de Speaker Recognition para Javascript

---
+++ speech_recognition_test/speech_recognition.py
@@ -0,0 +1,8 @@
+import azure.cognitiveservices.speech as speechsdk
+import time
+import pickle
+from typing import List
+
+# Credenciales para el servicio
+SUBSCRIPTION_KEY = "[SUBSCRIPTION_KEY]"
+SERVICE_REGION = "[SERVICE_REGION]"
